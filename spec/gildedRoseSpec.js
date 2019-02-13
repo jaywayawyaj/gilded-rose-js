a19d01651239740ac379d2928e679958a44c5d0a
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-13 20:43:20 +0000

    more tests written and passing

--- spec/gildedRoseSpec.js
+++ spec/gildedRoseSpec.js
@@ -3,9 +3,21 @@ describe("Gilded Rose", function() {
 
   describe("#updateQuality", function() {
 
+    it("return items name", function() {
+      const gildedRose = new Shop([ new Item("foo", 5, 4) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toEqual("foo");
+    });
+
+    it("should reduce sellIn by 1", function() {
+      const gildedRose = new Shop([ new Item("foo", 5, 4) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(4);
+    });
+
     it("No product can have negative quality", function() {
       const gildedRose = new Shop([
-         new Item("foo", 1, 0),
+         new Item("foo", 0, 0),
          new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)
         ]);
       const items = gildedRose.updateQuality();
@@ -13,15 +25,17 @@ describe("Gilded Rose", function() {
       expect(items[1].quality).toEqual(0);
     });
 
+    it("No product can have quality above 50", function() {
+      const gildedRose = new Shop([
+         new Item("Aged Brie", 5, 50),
+         new Item("Backstage passes to a TAFKAL80ETC concert", 6, 49)
+        ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(50);
+      expect(items[1].quality).toEqual(50);
+    });
 
     describe("Regular product", function() {
-
-      it("should reduce sellIn by 1", function() {
-        const gildedRose = new Shop([ new Item("foo", 5, 4) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("foo");
-        expect(items[0].sellIn).toEqual(4);
-      });
 
       it("should reduce quality by 1", function() {
         const gildedRose = new Shop([ new Item("foo", 5, 4) ]);
@@ -30,8 +44,8 @@ describe("Gilded Rose", function() {
         expect(items[0].quality).toEqual(3);
       });
 
-      it("should reduce quality by 1", function() {
-        const gildedRose = new Shop([ new Item("foo", 5, 4) ]);
+      it("quality reduction is doubled after sellIn is 0", function() {
+        const gildedRose = new Shop([ new Item("foo", 0, 5) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].name).toEqual("foo");
         expect(items[0].quality).toEqual(3);
@@ -40,30 +54,27 @@ describe("Gilded Rose", function() {
 
     describe("Aged Brie", function() {
 
-      xit("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      it("Increase quality as it ages", function() {
+        const gildedRose = new Shop([ new Item("Aged Brie", 1, 1) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        expect(items[0].quality).toEqual(2);
       });
 
-      xit("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      it("Ages doubly fast after sellIn hits 0", function() {
+        const gildedRose = new Shop([ new Item("Aged Brie", 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        expect(items[0].quality).toEqual(2);
       });
     });
 
     describe("Legendary", function() {
-      xit("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      it("Remains unchanged", function() {
+        const gildedRose = new Shop(
+          [ new Item("Sulfuras, Hand of Ragnaros", 5, 5) ]
+        );
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-      });
-
-      xit("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        expect(items[0].sellIn).toEqual(5);
+        expect(items[0].sellIn).toEqual(5);
       });
     });
 
