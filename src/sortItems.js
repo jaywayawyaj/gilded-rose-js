ab312f577873297019b40879096e44312911278b
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-15 15:10:52 +0000

    new class - updateSellIn

--- src/sortItems.js
+++ src/sortItems.js
@@ -1,6 +1,7 @@
 class SortItems {
-  constructor(updateQuality = new UpdateQuality) {
+  constructor(updateQuality=new UpdateQuality, updateSellIn=new UpdateSellIn) {
     this.updateQuality = updateQuality;
+    this.updateSellIn = updateSellIn;
   }
 
   parseItems(items) {
@@ -13,7 +14,7 @@ class SortItems {
   determineType(item) {
     switch(item.name) {
       case "Aged Brie":
-        this.reduceSellIn(item);
+        this.updateSellIn.reduceSellIn(item);
         this.updateQuality.updateAged(item);
         return item;
         break;
@@ -21,24 +22,24 @@ class SortItems {
         return item;
         break;
       case "Backstage passes to a TAFKAL80ETC concert":
-        this.reduceSellIn(item);
+        this.updateSellIn.reduceSellIn(item);
         this.updateQuality.updateBackstagePasses(item);
         return item;
         break;
       case "Conjured":
-        this.reduceSellIn(item);
+        this.updateSellIn.reduceSellIn(item);
         this.updateQuality.updateConjured(item);
         return item;
         break;
       default:
-        this.reduceSellIn(item);
+        this.updateSellIn.reduceSellIn(item);
         this.updateQuality.updateItem(item);
         return item;
     }
   }
 
-  reduceSellIn(item) {
-    item.sellIn = (item.sellIn > 0 ? item.sellIn - 1 : item.sellIn = -1);
-    return item;
-  }
+  // reduceSellIn(item) {
+  //   item.sellIn = (item.sellIn > 0 ? item.sellIn - 1 : item.sellIn = -1);
+  //   return item;
+  // }
 }
---
+++ src/updateSellIn.js
@@ -0,0 +1,6 @@
+class UpdateSellIn {
+  reduceSellIn(item) {
+    item.sellIn = (item.sellIn > 0 ? item.sellIn - 1 : item.sellIn = -1);
+    return item;
+  }
+}
