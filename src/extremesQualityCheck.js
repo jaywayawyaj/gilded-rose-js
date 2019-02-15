5a2488ff666e9d5a6c4a59d5415ea0988062429e
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-15 17:19:23 +0000

    removed magic numbers

--- src/extremesQualityCheck.js
+++ src/extremesQualityCheck.js
@@ -1,10 +1,18 @@
 class ExtremesQualityCheck {
+  constructor() {
+    this.MIN_QUALITY = 0
+    this.MAX_QUALITY = 50
+  }
 
   minQuality(item) {
-    item.quality = (item.quality <= 0 ? item.quality = 0 : item.quality)
+    item.quality = (
+      item.quality <= this.MIN_QUALITY ? this.MIN_QUALITY : item.quality
+    )
   }
 
   maxQuality(item) {
-    item.quality = (item.quality >= 50 ? item.quality = 50 : item.quality);
+    item.quality = (
+      item.quality >= this.MAX_QUALITY ? this.MAX_QUALITY = 50 : item.quality
+    );
   }
 }
--- src/updateQuality.js
+++ src/updateQuality.js
@@ -1,44 +1,73 @@
 class UpdateQuality {
   constructor(extremesQualityCheck = new ExtremesQualityCheck) {
     this.check = extremesQualityCheck;
+    this.MIN_QUALITY = 0
+    this.MAX_QUALITY = 50
   }
 
   updateItem(item) {
     if (item.sellIn < 0) {
-      item.quality = (item.quality > 0 ? item.quality - 2 : item.quality = 0);
+      item.quality = (
+        item.quality > this.MIN_QUALITY ?
+        item.quality - 2 : item.quality = this.MIN_QUALITY
+      );
     } else {
-      item.quality = (item.quality > 0 ? item.quality - 1 : item.quality = 0);
+      item.quality = (
+        item.quality > this.MIN_QUALITY ?
+        item.quality - 1 : item.quality = this.MIN_QUALITY
+      );
     }
     this.check.minQuality(item);
   }
 
   updateAged(item) {
     if (item.sellIn < 0) {
-      item.quality = (item.quality < 50 ? item.quality + 2 : item.quality = 50);
+      item.quality = (
+        item.quality < this.MAX_QUALITY ?
+        item.quality + 2 : item.quality = this.MAX_QUALITY
+      );
     } else {
-      item.quality = (item.quality < 50 ? item.quality + 1 : item.quality = 50);
+      item.quality = (
+        item.quality < this.MAX_QUALITY ?
+        item.quality + 1 : item.quality = this.MAX_QUALITY
+      );
     }
     this.check.maxQuality(item);
   }
 
   updateBackstagePasses(item) {
     if (item.sellIn <= 10 && item.sellIn > 5) {
-      item.quality = (item.quality < 50 ? item.quality + 2 : item.quality = 50);
+      item.quality = (
+        item.quality < this.MAX_QUALITY ?
+        item.quality + 2 : item.quality = this.MAX_QUALITY
+      );
     } else if (item.sellIn <= 5 && item.sellIn >= 0) {
-      item.quality = (item.quality < 50 ? item.quality + 3 : item.quality = 50);
-    } else if ( item.sellIn < 0 ) {
-      item.quality = 0;
+      item.quality = (
+        item.quality < this.MAX_QUALITY ?
+        item.quality + 3 : item.quality = this.MAX_QUALITY
+      );
+    } else if ( item.sellIn < this.MIN_QUALITY) {
+      item.quality = this.MIN_QUALITY;
     } else {
-      item.quality = (item.quality < 50 ? item.quality + 1 : item.quality = 50);
+      item.quality = (
+        item.quality < this.MAX_QUALITY ?
+        item.quality + 1 : item.quality = this.MAX_QUALITY
+      );
     }
     this.check.maxQuality(item);
   }
 
   updateConjured(item) {
     if (item.sellIn < 0) {
-      item.quality = (item.quality > 0 ? item.quality - 4 : item.quality = 0);
+      item.quality = (
+        item.quality > this.MIN_QUALITY ?
+        item.quality - 4 : item.quality = this.MIN_QUALITY
+      );
     } else {
-      item.quality = (item.quality > 0 ? item.quality - 2 : item.quality = 0);
+      item.quality = (
+        item.quality > this.MIN_QUALITY ?
+        item.quality - 2 : item.quality = this.MIN_QUALITY
+      );
     }
     this.check.minQuality(item);
   }
