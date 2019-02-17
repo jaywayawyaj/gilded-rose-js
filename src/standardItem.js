2b466649de392fc193d6932c279cf71899a9376d
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-17 09:12:46 +0000

    made ternary operators more precise

---
+++ src/standardItem.js
@@ -0,0 +1,20 @@
+class StandardItem {
+  constructor(extremesQualityCheck = new ExtremesQualityCheck) {
+    this.check = extremesQualityCheck;
+  }
+  
+  updateQuality(item) {
+    if (item.sellIn < 0) {
+      item.quality = (
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality - 2 : item.quality = this.check.MIN_QUALITY
+      );
+    } else {
+      item.quality = (
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality - 1 : item.quality = this.check.MIN_QUALITY
+      );
+    }
+    this.check.minQuality(item);
+  }
+}
--- src/updateQuality.js
+++ src/updateQuality.js
@@ -1,20 +1,16 @@
 class UpdateQuality {
   constructor(extremesQualityCheck = new ExtremesQualityCheck) {
     this.check = extremesQualityCheck;
-    this.MIN_QUALITY = 0
-    this.MAX_QUALITY = 50
   }
 
   updateItem(item) {
     if (item.sellIn < 0) {
-      item.quality = (
-        item.quality > this.MIN_QUALITY ?
-        item.quality - 2 : item.quality = this.MIN_QUALITY
-      );
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality -= 2 : item.quality = this.check.MIN_QUALITY;
     } else {
       item.quality = (
-        item.quality > this.MIN_QUALITY ?
-        item.quality - 1 : item.quality = this.MIN_QUALITY
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality - 1 : item.quality = this.check.MIN_QUALITY
       );
     }
     this.check.minQuality(item);
@@ -22,52 +18,38 @@ class UpdateQuality {
 
   updateAged(item) {
     if (item.sellIn < 0) {
-      item.quality = (
-        item.quality < this.MAX_QUALITY ?
-        item.quality + 2 : item.quality = this.MAX_QUALITY
-      );
+        item.quality < this.check.MAX_QUALITY ?
+        item.quality += 2 : item.quality = this.check.MAX_QUALITY;
     } else {
-      item.quality = (
-        item.quality < this.MAX_QUALITY ?
-        item.quality + 1 : item.quality = this.MAX_QUALITY
-      );
+        item.quality < this.check.MAX_QUALITY ?
+        item.quality += 1 : item.quality = this.check.MAX_QUALITY;
     }
     this.check.maxQuality(item);
   }
 
   updateBackstagePasses(item) {
     if (item.sellIn <= 10 && item.sellIn > 5) {
-      item.quality = (
-        item.quality < this.MAX_QUALITY ?
-        item.quality + 2 : item.quality = this.MAX_QUALITY
-      );
+      item.quality < this.check.MAX_QUALITY ?
+        item.quality += 2 : item.quality = this.check.MAX_QUALITY;
     } else if (item.sellIn <= 5 && item.sellIn >= 0) {
-      item.quality = (
-        item.quality < this.MAX_QUALITY ?
-        item.quality + 3 : item.quality = this.MAX_QUALITY
-      );
-    } else if ( item.sellIn < this.MIN_QUALITY) {
-      item.quality = this.MIN_QUALITY;
+        item.quality < this.check.MAX_QUALITY ?
+        item.quality += 3 : item.quality = this.check.MAX_QUALITY;
+    } else if ( item.sellIn < this.check.MIN_QUALITY) {
+      item.quality = this.check.MIN_QUALITY;
     } else {
-      item.quality = (
-        item.quality < this.MAX_QUALITY ?
-        item.quality + 1 : item.quality = this.MAX_QUALITY
-      );
+        item.quality < this.check.MAX_QUALITY ?
+        item.quality += 1 : item.quality = this.check.MAX_QUALITY;
     }
     this.check.maxQuality(item);
   }
 
   updateConjured(item) {
     if (item.sellIn < 0) {
-      item.quality = (
-        item.quality > this.MIN_QUALITY ?
-        item.quality - 4 : item.quality = this.MIN_QUALITY
-      );
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality -= 4 : item.quality = this.check.MIN_QUALITY;
     } else {
-      item.quality = (
-        item.quality > this.MIN_QUALITY ?
-        item.quality - 2 : item.quality = this.MIN_QUALITY
-      );
+        item.quality > this.check.MIN_QUALITY ?
+        item.quality -= 2 : item.quality = this.check.MIN_QUALITY;
     }
     this.check.minQuality(item);
   }
