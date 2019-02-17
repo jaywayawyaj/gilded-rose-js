faa931e0025d0f509e3eba26f1546e70bdd5ea89
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-17 09:14:36 +0000

    more ternary precision

--- src/updateSellIn.js
+++ src/updateSellIn.js
@@ -1,5 +1,5 @@
 class UpdateSellIn {
   reduceSellIn(item) {
-    item.sellIn = (item.sellIn > 0 ? item.sellIn - 1 : item.sellIn = -1);
+    item.sellIn > 0 ? item.sellIn -= 1 : item.sellIn = -1;
   }
 }
